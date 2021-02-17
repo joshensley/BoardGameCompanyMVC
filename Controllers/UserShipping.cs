8c58a49bc1ae301974d1dc69dc05b7c0fe9c3635
Josh Hensley <joshensley@gmail.com>
2021-02-17 16:09:09 -0600

    Created User Shipping Index Page

---
+++ Controllers/UserShipping.cs
@@ -0,0 +1,55 @@
+﻿using BoardGameCompanyMVC.Data;
+using BoardGameCompanyMVC.Models;
+using BoardGameCompanyMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardGameCompanyMVC.Controllers
+{
+    public class UserShipping : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public UserShipping(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index(int? pageNumber)
+        {
+            IQueryable<UserOrderGroup> data =
+                from inventoryItems in _db.InventoryItems
+                where inventoryItems.OrderedDate != null
+                    && inventoryItems.ShippingNumber == null
+                    && inventoryItems.InStock == false
+                join boardGame in _db.BoardGames
+                    on inventoryItems.BoardGameID equals boardGame.ID
+                group inventoryItems by new
+                {
+                    inventoryItems.OrderNumber,
+                    inventoryItems.OrderedDate,
+
+                } into userOrderGroupIQ
+                orderby userOrderGroupIQ.Key.OrderedDate ascending
+                select new UserOrderGroup()
+
+                {
+                    OrderNumber = (int)userOrderGroupIQ.Key.OrderNumber,
+                    OrderDate = userOrderGroupIQ.Key.OrderedDate,
+                    OrderCount = userOrderGroupIQ.Count()
+                };
+            
+            int pageSize = 5;
+            return View(await PaginatedList<UserOrderGroup>.CreateAsync(data.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/UserOrderGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardGameCompanyMVC.ViewModels
+{
+    public class UserOrderGroup
+    {
+
+
+        [Display(Name = "Order #")]
+        public int OrderNumber { get; set; }
+
+        [Display(Name = "Order Date")]
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+
+        [Display(Name = "Orders")]
+        public int OrderCount { get; set; }
+
+        [Display(Name = "Board Game ID")]
+        public int BoardGameID { get; set; }
+
+        [Display(Name = "Title")]
+        public string BoardGameTitle { get; set; }
+
+        [Display(Name = "Image File Path")]
+        public string BoardGameImageFilePath { get; set; }
+    }
+}
