b6e30ced801a583fdd60a079a484872769b93260
Josh Hensley <joshensley@gmail.com>
2021-02-22 16:50:42 -0600

    Default is Admin is added when database is intialized for the first time.

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,65 @@
+﻿using BoardGameCompanyMVC.Models;
+using BoardGameCompanyMVC.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardGameCompanyMVC.Data
+{
+   
+
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(
+            ApplicationDbContext db, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public async void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(r => r.Name == SD.AdminEndUser)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.AdminEndUser)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName="joshensley@gmail.com",
+                Email="joshensley@gmail.com",
+                Name="Josh Hensley",
+                EmailConfirmed=true,
+                PhoneNumber="1112223333"
+
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            ApplicationUser user = await _db.Users.FirstOrDefaultAsync(u => u.Email == "joshensley@gmail.com");
+
+            _userManager.AddToRoleAsync(user, SD.AdminEndUser).GetAwaiter().GetResult();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace BoardGameCompanyMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDbInitializer, DbInitializer>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -45,7 +46,7 @@ namespace BoardGameCompanyMVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
             StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["SecretKey"];
 
@@ -60,6 +61,7 @@ namespace BoardGameCompanyMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            dbInitializer.Initialize();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
